b2a041b3082baefa3759c63320f0c7d502b23459
Frederico <fredcsouza@gmail.com>
2018-11-21 23:34:08 -0200

    adicionando criação de grid

--- designs.js
+++ designs.js
@@ -1,11 +1,25 @@
 // Select color input
+let colorPicker = $('#colorPicker');
+
 // Select size input
+let innerWidth = $('#inputWidth');
+let innerHeight = $('#inputHeight');
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
+  let table = $('#pixelCanvas');
+  table.find('tr').remove();
+  for (let i = 1; i <= innerHeight.val(); i++) {
+    let tr = $('<tr>');
+    for (let i = 1; i <= innerWidth.val(); i++) {
+      $(tr).append('<td width="20px" height="20px"></td>');
+    }
+    table.append(tr);
+  }
 };
 
-
-
+$('#sizePicker').submit(function (e) {
+  e.preventDefault();
+  makeGrid();
+});
